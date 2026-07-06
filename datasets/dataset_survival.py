7e8d45aeb846fe7c32f372e587b6528d3dc48955
Valentin <val.bosch2001@gmail.com>
2026-07-06 10:26:04 +0200

    up

--- datasets/dataset_survival.py
+++ datasets/dataset_survival.py
@@ -259,7 +259,8 @@ class Generic_MIL_Survival_Dataset(Generic_WSI_Survival_Dataset):
                     path_features = []
                     from datasets.BatchWSI import BatchWSI
                     for slide_id in slide_ids:
-                        wsi_path = os.path.join(data_dir, 'graph_euclidean_files', '{}.pt'.format(slide_id.rstrip('.svs')))
+                        slide_name = str(slide_id) if isinstance(slide_id, str) else str(int(slide_id))
+                        wsi_path = os.path.join(data_dir, '{}.pt'.format(slide_name.rstrip('.svs')))
                         wsi_bag = torch.load(wsi_path)
                         path_features.append(wsi_bag)
 
